11b1b0b63c6323594124e16a465e5cc5c5747453
TenzinDala <tenzindala2000@gmail.com>
2023-07-16 07:45:00 -0400

    Add project files.

---
+++ PRG-Assignment-LAB06/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PRG_Assignment_LAB06
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.lblTeacher = new System.Windows.Forms.Label();
+            this.btnStudentLogin = new System.Windows.Forms.Button();
+            this.btnTeacherLogin = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudent.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblStudent.Location = new System.Drawing.Point(247, 139);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(193, 33);
+            this.lblStudent.TabIndex = 0;
+            this.lblStudent.Text = "Student Login";
+            // 
+            // lblTeacher
+            // 
+            this.lblTeacher.AutoSize = true;
+            this.lblTeacher.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeacher.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTeacher.Location = new System.Drawing.Point(247, 273);
+            this.lblTeacher.Name = "lblTeacher";
+            this.lblTeacher.Size = new System.Drawing.Size(201, 33);
+            this.lblTeacher.TabIndex = 1;
+            this.lblTeacher.Text = "Teacher Login";
+            // 
+            // btnStudentLogin
+            // 
+            this.btnStudentLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnStudentLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudentLogin.Location = new System.Drawing.Point(292, 193);
+            this.btnStudentLogin.Name = "btnStudentLogin";
+            this.btnStudentLogin.Size = new System.Drawing.Size(110, 48);
+            this.btnStudentLogin.TabIndex = 2;
+            this.btnStudentLogin.Text = "Login";
+            this.btnStudentLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnTeacherLogin
+            // 
+            this.btnTeacherLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnTeacherLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeacherLogin.Location = new System.Drawing.Point(292, 322);
+            this.btnTeacherLogin.Name = "btnTeacherLogin";
+            this.btnTeacherLogin.Size = new System.Drawing.Size(110, 51);
+            this.btnTeacherLogin.TabIndex = 3;
+            this.btnTeacherLogin.Text = "Login";
+            this.btnTeacherLogin.UseVisualStyleBackColor = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Black;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitle.Location = new System.Drawing.Point(164, 35);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(358, 73);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Login Page";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnTeacherLogin);
+            this.Controls.Add(this.btnStudentLogin);
+            this.Controls.Add(this.lblTeacher);
+            this.Controls.Add(this.lblStudent);
+            this.Name = "mainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.Label lblTeacher;
+        private System.Windows.Forms.Button btnStudentLogin;
+        private System.Windows.Forms.Button btnTeacherLogin;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
